cd651bdacf00b5a70b507ed2844536b147b3c088
FJgonzalezmejia <fjgonzalezmejia9@gmail.com>
2024-07-22 09:36:22 -0600

    Add files via upload

---
+++ bookingsystem/bookingsystem/src/test/java/com/yourname/bookingsystem/BookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourname.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
